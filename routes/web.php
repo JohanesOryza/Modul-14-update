df9806b0561cfef078070c523ee068c8876e0f5b
JohanesOryza <oryzajohanes7@gmail.com>
2024-12-13 22:41:13 +0700

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\Auth\LoginController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('home', [HomeController::class, 'index'])->name('home');
+Route::get('profile', ProfileController::class)->name('profile');
+Route::resource('employees', EmployeeController::class);
+
+Route::get('/employees/{id}/edit', [EmployeeController::class, 'edit'])->name('employees.edit');
+Route::put('/employees/{id}', [EmployeeController::class, 'update'])->name('employees.update');
+Route::get('employees/{id}/edit', [EmployeeController::class, 'edit'])->name('employees.edit');
+Route::get('employees/{id}', [EmployeeController::class, 'show'])->name('employees.show');
+Route::delete('employees/{id}', [EmployeeController::class, 'destroy'])->name('employees.destroy');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::post('/login', [LoginController::class, 'authenticate']);
